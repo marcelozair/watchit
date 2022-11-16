50a253b74109e1b65b1e368b16655ca1af9b64d1
marcelozair <marceloberrocal2004@gmail.com>
2022-11-15 19:07:47 -0600

    Added and configured prettier with eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="py-10 bg-red-500">
-      
-    </div>
-  )
+  return <div className="py-10 bg-red-500"></div>;
 }
 
-export default App
+export default App;
